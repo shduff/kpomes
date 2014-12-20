aa76f8dfd0479a38c8cc7927f3be81044442297e
shaunalynn <shaunalynn@thesprouts.org>
2014-12-20 10:37:41 -0500

    staticheroku debugging

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ var path = require('path');
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+// app.get('/', function(request, response) {
+//   response.send('Hello World!');
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port);
