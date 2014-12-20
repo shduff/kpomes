76026fb390d120c587878743a2597d6be802e073
shaunalynn <shaunalynn@thesprouts.org>
2014-12-20 09:49:11 -0500

    staticheroku reorg, color recognition tests
    final style edits, began color recognition for cards, reorg for static
    heroku deployment

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//app.use(express.static(__dirname)); // Current directory is root
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on port', port);
